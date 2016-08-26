46edd5487e10351209e9dbc3eb765f0ddc655b27
Vladimir Dranik <v.dranik@gmail.com>
2016-08-27 02:33:42 +0300

    add_stencil

--- app/src/main/java/ua/com/vdranik/hw_week3/MainActivity.java
+++ app/src/main/java/ua/com/vdranik/hw_week3/MainActivity.java
@@ -1,10 +1,15 @@
 package ua.com.vdranik.hw_week3;
 
-import android.graphics.ColorMatrixColorFilter;
-import android.graphics.drawable.Drawable;
-import android.support.v4.content.res.ResourcesCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,7 +17,7 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView imageView;
-    private Drawable myDrawable;
+    private Bitmap image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,40 +25,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         imageView = (ImageView) findViewById(R.id.imageView);
-        Button standartButton = (Button) findViewById(R.id.standartButton);
-        Button invertButton = (Button) findViewById(R.id.invertButton);
-        standartButton.setOnClickListener(this);
-        invertButton.setOnClickListener(this);
-        myDrawable = ResourcesCompat.getDrawable(getResources(), R.drawable.rgb, null);
-        myDrawable.setColorFilter(new ColorMatrixColorFilter(STANDART)); // do nothing
-        imageView.setImageDrawable(myDrawable);
+        image = BitmapFactory.decodeResource(getResources(), R.drawable.smile);
+        Button origSmile = (Button) findViewById(R.id.origSmile);
+        Button cropSmile = (Button) findViewById(R.id.cropSmile);
+        origSmile.setOnClickListener(this);
+        cropSmile.setOnClickListener(this);
     }
-
-    private static final float[] STANDART = {
-            1.0f, 0, 0, 0, 0,     // red
-            0, 1.0f, 0, 0, 0,      // green
-            0, 0, 1.0f, 0, 0,      // blue
-            0, 0, 0, 1.0f, 0       // alpha
-    };
-
-    private static final float[] INVERT_GREEN_AND_RED = {
-            1.0f, 0, 0, 0, 0,  // red
-            0, -1.0f, 0, 0, 255,     // green
-            0, 0, -1.0f, 0, 255,   // blue
-            0, 0, 0, 1.0f, 0  // alpha
-    };
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.standartButton:
-                myDrawable.setColorFilter(new ColorMatrixColorFilter(STANDART));
-                imageView.setImageDrawable(myDrawable);
+            case R.id.cropSmile:
+                Bitmap cropped = getRoundedCroppedImage(image);
+                imageView.setImageBitmap(cropped);
                 break;
-            case R.id.invertButton:
-                myDrawable.setColorFilter(new ColorMatrixColorFilter(INVERT_GREEN_AND_RED));
-                imageView.setImageDrawable(myDrawable);
+            case R.id.origSmile:
+                imageView.setImageBitmap(image);
                 break;
         }
+    }
+
+    private Bitmap getRoundedCroppedImage(Bitmap bmp) {
+        int widthLight = bmp.getWidth();
+        int heightLight = bmp.getHeight();
+
+        Bitmap output = Bitmap.createBitmap(widthLight, heightLight, Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(output);
+        Paint paint = new Paint();
+        paint.setFlags(Paint.ANTI_ALIAS_FLAG);
+
+        RectF rectF = new RectF(new Rect(0, 0, widthLight, heightLight));
+
+        canvas.drawRoundRect(rectF, widthLight / 2 ,heightLight / 2,paint);
+
+        Paint paintImage = new Paint();
+        paintImage.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP));
+        canvas.drawBitmap(bmp, 0, 0, paintImage);
+
+        return output;
     }
 }
